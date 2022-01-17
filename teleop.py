6719b5b8ba2fab38bc7274a1581dd672db525dd4
krzysztof-nowicki <67326227+krzysztof-nowicki@users.noreply.github.com>
2022-01-17 14:29:09 +0100

    working smooth teleoperation

--- teleop.py
+++ teleop.py
@@ -27,8 +27,8 @@ import tf2_msgs.msg
 import tensorflow as tf2
 import tensorflow_hub as hub
 
-#from openpose.body.estimator import BodyPoseEstimator
-#from openpose.utils import draw_body_connections, draw_keypoints
+from openpose.body.estimator import BodyPoseEstimator
+from openpose.utils import draw_body_connections, draw_keypoints
 
 import cmath
 import math
@@ -72,6 +72,14 @@ global angle4, angle5, angle6
 
 global image_height
 
+global tmp5, tmp6
+
+global fulltime1, fulltime2
+
+tmp5=0
+tmp6=0
+fulltime1=0
+fulltime2=0
 #CALIBRATION FUNCTIONS
 
 def calibrate_camera(images_folder):
@@ -247,9 +255,9 @@ P2 = mtx2 @ RT2 #projection matrix for C2
 #loading model
 rospy.loginfo('Loading model')
 
-#estimator = BodyPoseEstimator(pretrained=True)
+estimator = BodyPoseEstimator(pretrained=True)
 model = hub.load('https://tfhub.dev/google/movenet/singlepose/thunder/4')
-#model = hub.load('https://tfhub.dev/google/movenet/singlepose/lightning/3')
+#model = hub.load('https://tfhub.dev/google/movenet/singlepose/lightning/4')
 movenet = model.signatures['serving_default']
 
 rospy.loginfo('Model loaded')
@@ -262,7 +270,7 @@ bridge = CvBridge()
 def process_image_cam1(msg):
 
     time_movecam1 = time.time()
-    global tmp1, tmp3
+    global tmp1, tmp3, tmp5, fulltime1
     global points2
 
     global points4
@@ -277,13 +285,13 @@ def process_image_cam1(msg):
     global image_height
     img = bridge.imgmsg_to_cv2(msg, "bgr8")
     
-
     '''
     h1, w1 = img.shape[:2] 
     print("wymiary kamery cam1:")
     print(w1)
     print(h1)
     print("koniec kamery cam1:")
+    
     '''
     y, x, _ = img.shape
     image_height = y
@@ -344,7 +352,7 @@ def process_image_cam1(msg):
     '''
     #image_dst = draw_body_connections(img, keypoints, thickness=4, alpha=0.7)
     #image_dst = draw_keypoints(image_dst, keypoints, radius=5, alpha=0.8)
-    
+
     for k in keypoints[0,0,:,:]:
         # Converts to numpy array
         k = k.numpy()
@@ -355,8 +363,7 @@ def process_image_cam1(msg):
 
         image_dst = cv2.circle(img, (xc, yc), 2, (0, 255, 0), 5)
             
-            
-      
+
     image_message = bridge.cv2_to_imgmsg(image_dst, "bgr8")
     #print("publikacja cam1")
     image_pub_cam1.publish(image_message)
@@ -365,14 +372,34 @@ def process_image_cam1(msg):
     	tmp1 = 1
     time_movecam11 = time.time()
     time_interval_cam1 = time_movecam11 - time_movecam1
-    print("camera 1 feedback time:")
-    print(time_interval_cam1)
+    if tmp5 < 200:
+        fulltime1= fulltime1+time_interval_cam1
+        tmp5=tmp5+1
+    elif tmp5 == 200:
+        print(fulltime1) 
+    #print("camera 1 feedback time:")
+    #print(time_interval_cam1)
     tmp3=1
 
 def process_image_cam2(msg):
-
+    global angle1, angle2, angle3
+    global angle4, angle5, angle6
+    
+    global points1
+    global points2
+    global points3
+    global points4
+    global points5
+    global points6
+    global points7
+    global points8
+    global points9
+    global points10
+    global points11
+    global points12
+    
     time_movecam2 = time.time()
-    global tmp2, tmp4
+    global tmp2, tmp4, tmp1, tmp6, fulltime2
     global points1
 
     global points3
@@ -394,8 +421,7 @@ def process_image_cam2(msg):
     # resize image
     img = cv2.resize(img, dim, interpolation = cv2.INTER_AREA)
     img = img[:, 107:747]
-    
-  
+
     y, x, _ = img.shape
     
     # A frame of video or an image, represented as an int32 tensor of shape: 256x256x3. Channels order: RGB with values in [0, 255].
@@ -427,6 +453,7 @@ def process_image_cam2(msg):
     hand2_x = int(keypoints2[0, 0, 7, 1]* x)
     hand3_y = int(keypoints2[0, 0, 9, 0]* y)
     hand3_x = int(keypoints2[0, 0, 9, 1]* x)
+
     #keypoints = estimator(img)
 
     
@@ -451,7 +478,7 @@ def process_image_cam2(msg):
     '''
     #image_dst = draw_body_connections(img, keypoints, thickness=4, alpha=0.7)
     #image_dst = draw_keypoints(image_dst, keypoints, radius=5, alpha=0.8)
-    
+
     for k in keypoints[0,0,:,:]:
         # Converts to numpy array
         k = k.numpy()
@@ -461,7 +488,7 @@ def process_image_cam2(msg):
         xc = int(k[1] * x)
 
         image_dst = cv2.circle(img, (xc, yc), 2, (0, 255, 0), 5)
-         
+  
     image_message = bridge.cv2_to_imgmsg(image_dst, "bgr8")
     #print("publikacja cam2")
     image_pub_cam2.publish(image_message)
@@ -470,9 +497,90 @@ def process_image_cam2(msg):
     	tmp2=1
     time_movecam22 = time.time()
     time_interval_cam2 = time_movecam22 - time_movecam2
-    print("camera 2 feedback time:")
-    print(time_interval_cam2)
+    print("iteration: ")
+    print(tmp6)
+    if tmp6 < 1000:
+        fulltime2= fulltime2+time_interval_cam2
+        tmp6=tmp6+1
+    elif tmp6 == 1000:
+        print(fulltime2)    
+    #print("camera 2 feedback time:")
+    #print(time_interval_cam2)
     tmp4=1
+    print("Conditions for triangulation check:")
+    print(tmp1)
+    print(tmp2)
+        
+        #p3ds = []
+        
+        
+    if tmp1!=0 and tmp2!=0:
+
+        p_shoulder = DLT(P1, P2, points1, points2)
+        p_elbow = DLT(P1, P2, points3, points4)
+        p_wrist = DLT(P1, P2, points5, points6)
+        p_hand1 = DLT(P1, P2, points7, points8)
+        p_hand2 = DLT(P1, P2, points9, points10)
+        p_hand3 = DLT(P1, P2, points11, points12)
+            
+        x1=p_elbow[0]-p_shoulder[0] 
+        y1=p_elbow[1]-p_shoulder[1] 
+        z1=p_elbow[2]-p_shoulder[2]
+        x2=p_wrist[0]-p_elbow[0] 
+        y2=p_wrist[1]-p_elbow[1] 
+        z2=p_wrist[2]-p_elbow[2]
+
+        x3=p_hand2[0]-p_hand1[0]
+        y3=p_hand2[1]-p_hand1[1]
+        z3=p_hand2[2]-p_hand1[2]
+        x4=p_hand3[0]-p_hand2[0]
+        y4=p_hand3[1]-p_hand2[1]
+        z4=p_hand3[2]-p_hand2[2]
+   
+            #fig = plt.figure(figsize=(4,4))
+            #ax = fig.add_subplot(111, projection='3d')
+            #ax.scatter(p_shoulder[0], p_shoulder[1], p_shoulder[2], color='red')
+            #ax.scatter(p_elbow[0], p_elbow[1], p_elbow[2], color='blue')
+            #ax.scatter(p_wrist[0], p_wrist[1], p_wrist[2], color='green')
+        #plt.show()
+        angle1=asin(y1/sqrt((x1*x1)+(y1*y1)+(z1*z1)))
+        if x1 > 0:
+            angle2=atan2(z1,x1)
+        elif x1 < 0:
+            angle2=atan2(z1,x1)
+        else :
+            angle2=pi/2
+
+        angle3=asin(y2/sqrt((x2*x2)+(y2*y2)+(z2*z2)))
+
+
+        angle4 = asin(z3 / sqrt((x3 * x3) + (y3 * y3) + (z3 * z3)))
+        if x1 > 0:
+            angle5 = atan2(y3, x3)
+        elif x1 < 0:
+            angle5 = atan2(y3, x3) + pi
+        else:
+            angle5 = pi / 2
+
+        angle6 = asin(z4 / sqrt((x4 * x4) + (y4 * y4) + (z4 * z4)))
+
+        angle1=-angle1
+        angle2=angle2
+        angle3=-angle3
+        angle4=angle4
+        angle5=angle5/1.5
+        angle6=angle6
+        print("Calculated angles for the move order:")
+        print(angle1)
+        print(angle2)
+        print(angle3)
+        print(angle4)
+        print(angle5)
+        print(angle6)
+        tmp1=0
+        tmp2=0  
+    ArmSimpleTrajectory()    
+    
 #UR5 CONTROL
 
 
@@ -568,20 +676,20 @@ class ArmSimpleTrajectory:
         #rospy.sleep(1)
         time_move3 = time.time()
         time_interval_move1 = time_move2 - time_move1
-        print("create order time:")
-        print(time_interval_move1)
+        #print("create order time:")
+        #print(time_interval_move1)
         time_interval_move2 = time_move3 - time_move2
-        print("send order time:")
-        print(time_interval_move2)
+        #print("send order time:")
+        #print(time_interval_move2)
 
 
 
 #Main loop
 itmp=0
 
-image_pub_cam1 = rospy.Publisher("keypoints_image_cam1",Image, queue_size=1)
+image_pub_cam1 = rospy.Publisher("keypoints_image_cam1",Image, queue_size=1000)
 
-image_pub_cam2 = rospy.Publisher("keypoints_image_cam2",Image, queue_size=1
+image_pub_cam2 = rospy.Publisher("keypoints_image_cam2",Image, queue_size=1000)
 
 arm_goal_pub = rospy.Publisher("/arm_controller/scaled_pos_joint_traj_controller/follow_joint_trajectory/goal",FollowJointTrajectoryActionGoal ,queue_size=1)
 rospy.init_node('teleop_arm')
@@ -631,6 +739,7 @@ if __name__ == '__main__':
     
     rate = rospy.Rate(1.0)
     while not rospy.is_shutdown():
+        
         time_0 = time.time()
         try:
             
@@ -651,115 +760,14 @@ if __name__ == '__main__':
 
         rospy.Subscriber("/cam1/image_raw", Image, process_image_cam1, queue_size=1, buff_size=2**24)
 
-        rospy.Subscriber("/cam2/image_raw", Image, process_image_cam2, queue_size=1, buff_size=2**24)
+        rospy.Subscriber("/cam2/image_raw", Image, process_image_cam2, queue_size=1,  buff_size=2**24)
         
         time_2 = time.time()
         rospy.wait_for_message("keypoints_image_cam1",Image)
         rospy.wait_for_message("keypoints_image_cam1",Image)
-        rospy.rostime.wallsleep(0.5)
-        #rospy.on_shutdown()
-        print("Conditions for triangulation check:")
-        print(tmp1)
-        print(tmp2)
-        
-        #p3ds = []
-        
-        
-        if tmp1!=0 and tmp2!=0:
-
-            p_shoulder = DLT(P1, P2, points1, points2)
-            p_elbow = DLT(P1, P2, points3, points4)
-            p_wrist = DLT(P1, P2, points5, points6)
-            p_hand1 = DLT(P1, P2, points7, points8)
-            p_hand2 = DLT(P1, P2, points9, points10)
-            p_hand3 = DLT(P1, P2, points11, points12)
-            
+        rospy.spin()
+        #rospy.rostime.wallsleep(0.5)
 
 
 
-            #POTENTIAL BUGS TO CHECK!!!
-            x1=p_elbow[0]-p_shoulder[0] 
-            y1=p_elbow[1]-p_shoulder[1] 
-            z1=p_elbow[2]-p_shoulder[2]
-            x2=p_wrist[0]-p_elbow[0] 
-            y2=p_wrist[1]-p_elbow[1] 
-            z2=p_wrist[2]-p_elbow[2]
-
-            x3=p_hand2[0]-p_hand1[0]
-            y3=p_hand2[1]-p_hand1[1]
-            z3=p_hand2[2]-p_hand1[2]
-            x4=p_hand3[0]-p_hand2[0]
-            y4=p_hand3[1]-p_hand2[1]
-            z4=p_hand3[2]-p_hand2[2]
-            
-            #fig = plt.figure(figsize=(4,4))
-            #ax = fig.add_subplot(111, projection='3d')
-            #ax.scatter(p_shoulder[0], p_shoulder[1], p_shoulder[2], color='red')
-            #ax.scatter(p_elbow[0], p_elbow[1], p_elbow[2], color='blue')
-            #ax.scatter(p_wrist[0], p_wrist[1], p_wrist[2], color='green')
-            #plt.show()
-            
-            
-
-            angle1=asin(y1/sqrt((x1*x1)+(y1*y1)+(z1*z1)))
-
-            if x1 > 0:
-                angle2=atan2(z1,x1)
-            elif x1 < 0:
-                angle2=atan2(z1,x1)
-            else :
-                angle2=pi/2
-
-            angle3=asin(y2/sqrt((x2*x2)+(y2*y2)+(z2*z2)))
-
-
-            angle4 = asin(z3 / sqrt((x3 * x3) + (y3 * y3) + (z3 * z3)))
-            if x1 > 0:
-                angle5 = atan2(y3, x3)
-            elif x1 < 0:
-                angle5 = atan2(y3, x3) + pi
-            else:
-                angle5 = pi / 2
-
-            angle6 = asin(z4 / sqrt((x4 * x4) + (y4 * y4) + (z4 * z4)))
-
-
-
-            angle1=-angle1
-            angle2=angle2
-            angle3=-angle3
-            angle4=angle4
-            angle5=angle5/1.5
-            angle6=angle6
-            print("Calculated angles for the move order:")
-            print(angle1)
-            print(angle2)
-            print(angle3)
-            print(angle4)
-            print(angle5)
-            print(angle6)
-            tmp1=0
-            tmp2=0
-        time_3 = time.time()  
-        ArmSimpleTrajectory()    
-        print("Debug times:")
-        time_4 = time.time()
-        time_interval0 = time_1 - time_0
-        print("forward kinematics time:")
-        print(time_interval0)
-        
-        time_interval1 = time_2 - time_1
-        
-        print("subscribers time:")
-        print(time_interval1)
-        
-        time_interval2 = time_3 - time_2
-        
-        print("triangulation time:")
-        print(time_interval2)
-        
-        time_interval3 = time_4 - time_3
-        
-        print("robot move time:")
-        print(time_interval3)
 
